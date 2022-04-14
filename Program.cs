20652a443d5f2b219cae8aaf3f9f860ea803b204
RISHABH <gguptarishabh82@gmail.com>
2022-04-14 16:22:51 +0530

    Adding Multiple AddressBooks with Unique Names

--- Program.cs
+++ Program.cs
@@ -11,17 +11,10 @@ namespace AddressBook_ADO.NET
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program");
-            string chooseOptionToAddContacts = string.Empty;
-            List<Contacts> book = new List<Contacts>();
-            do
-            {
-                Contacts C1 = new Contacts();
-                C1.AddContacts();
-                book.Add(C1);
-                Console.WriteLine("Enter the choice to add more persons Contacts in current Address Book: \n \"Yes\" or \"No\"");
-                chooseOptionToAddContacts = Console.ReadLine().ToUpper();
-            }
-            while (chooseOptionToAddContacts.Contains("Y"));
+            AddressBook addressBook = new AddressBook();
+            addressBook.AddContactsDetailinAddressBook();
+            addressBook.DisplayContactsInAddressBookDictionary();
+
         }
     }
 }
